f38dd8a56ab1c04238e584f1b14f2a184072a29a
Jueun-Park <elpion19@gmail.com>
2019-04-16 12:30:37 +0900

    edit variable name

--- RunningMedian.java
+++ RunningMedian.java
@@ -13,34 +13,34 @@ import java.util.Comparator;
 
 public class RunningMedian {
 	
-	public static void balanceTwoHeap(PriorityQueue<Integer> min_heap, PriorityQueue<Integer> max_heap, int num)
+	public static void balanceTwoHeap(PriorityQueue<Integer> left, PriorityQueue<Integer> right, int num)
 	{
 		/**
 		 * This method gets the integer and put it in heaps and balance two heaps.
-		 * @param min_heap	the left priority queue { @link java.util.PriorityQueue }
-		 * @param max_heap	the right priority queue { @link java.util.PriorityQueue }
+		 * @param left		max heap { @link java.util.PriorityQueue }
+		 * @param right		min heap { @link java.util.PriorityQueue }
 		 * @param num		an integer that has to be put into balanced two heaps
 		 * @return			return void
 		 */
-		min_heap.offer(num);
-		max_heap.offer(min_heap.poll());
-		if (min_heap.size() < max_heap.size()) {
-			min_heap.offer(max_heap.poll());
+		right.offer(num);
+		left.offer(right.poll());
+		if (left.size() > right.size()) {
+			right.offer(left.poll());
 		}
 	}
 	
-	public static double findMedian(PriorityQueue<Integer> min_heap, PriorityQueue<Integer> max_heap)
+	public static double findMedian(PriorityQueue<Integer> left, PriorityQueue<Integer> right)
 	{
 		/**
 		 * This method finds the median value in the number stream.
-		 * @param min_heap	the balanced left priority queue to find median { @link java.util.PriorityQueue }
-		 * @param max_heap	the balanced right priority queue to find median { @link java.util.PriorityQueue }
+		 * @param left		the balanced max heap to find median { @link java.util.PriorityQueue }
+		 * @param right		the balanced min heap to find median { @link java.util.PriorityQueue }
 		 * @return			return median value of the stream
 		 */
-		if (min_heap.size() > max_heap.size()) {
-			return (double)min_heap.peek();
+		if (left.size() < right.size()) {
+			return (double)right.peek();
 		} else {
-			return (min_heap.peek() + max_heap.peek()) / 2.0;
+			return (right.peek() + left.peek()) / 2.0;
 		}
 	}
 	
@@ -59,17 +59,14 @@ public class RunningMedian {
 			}
 			stream.add(current_number);
 			
-			balanceTwoHeap(min_heap, max_heap, current_number);
+			balanceTwoHeap(max_heap, min_heap, current_number);
 			
 			System.out.print("Number Stream: ");
 			for (int number: stream) {
 				System.out.print(number + ", ");
 			}			
-			System.out.println("-> Median: " + findMedian(min_heap, max_heap));
-			
+			System.out.println("-> Median: " + findMedian(max_heap, min_heap));
 		}
 		scanner.close();
-
 	}
-
 }
